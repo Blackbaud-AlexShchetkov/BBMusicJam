5c2abf9c1b0ce231f5d2c1b9fc9734633cacf8c4
Blackbaud-AlexWang <alex.wang@blackbaud.com>
2016-03-10 14:20:00 -0500

    fixed bcrypt dependency and added playlist model

---
+++ app/models/playlist.js
@@ -0,0 +1,22 @@
+// Pulls Mongoose dependency for creating schemas
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// Playlist Schema for storing tracks
+var PlaylistSchema = new Schema({
+	id: {type: String, required: true, unique: true}, // Spotify id for playlist
+	teamname: {type: String, required: true},
+	date: {type: Date, required: true},
+	theme: {type: String},
+	tracks: [{
+		id: {type: String, required: true, unique: true}, // Spotify id for track
+		name: {type: String, required: true},
+		artists: {type: [String], required: true},
+		album: {type: String, required: true},
+		username: {type: String, required: true}, // From users collection
+		points: {type: Number, default: 0}
+	}]
+});
+
+// Exports the PlaylistSchema for use elsewhere. Sets the MongoDB collection to be used as: "playlists" (collections will auto pluralize)
+module.exports = mongoose.model('playlist', PlaylistSchema);
