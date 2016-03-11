4d5a97294dc59dd44c4c5ba324088f084af3e6fe
Blackbaud-DavidKerr <david.kerr@blackbaud.com>
2016-03-11 07:01:33 -0500

    adding select playlist thing

---
+++ public/js/selectPlaylist/selectPlaylist.directive.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    'use strict';
+    angular.module('BBMusicJam').directive("selectPlaylist", [
+	    function () {
+	        return {
+	            restrict: 'E',
+	            replace: true,
+	            scope: {},
+	            templateUrl: 'js/selectPlaylist/selectPlaylistTemplate.html',
+	            //template: '<div>lol</div>',
+	            controller: [
+	                "$scope", "$http", "$cookies", function($scope, $http, $cookies) {
+	                    $scope.selectedItem = 'Coke';
+	                    $scope.swag = {
+	                        items: []
+	                    };
+	                    $scope.allTeams = [];
+                        $scope.getTeams = function() {
+                            $http.get('/teams', { params: { username: $cookies.get('user') } })
+                                .then(function (response) {
+                                $scope.swag.items = response.data;
+                                
+                            });
+                        }
+
+                        $scope.getPlaylist = function(teamname) {
+                            $cookies.put('playlist', teamname + '');
+                        }
+
+	                    $scope.getTeams();
+	                }
+	            ]
+	        };
+	    }
+    ]);
+})();
